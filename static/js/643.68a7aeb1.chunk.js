a96792f4e0881a8488419604a72702fd9c42467c
VladOlevskiy <99341001+VladOlevskiy@users.noreply.github.com>
2023-04-25 18:05:31 +0000

    Deploying to gh-pages from @ VladOlevskiy/Study-Center-Project@2454a980cd9b71776020e132921b58e2d768492b 🚀

--- static/js/643.68a7aeb1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[643],{8643:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)("h2",{children:"AboutPage 1212"})})}}}]);
-//# sourceMappingURL=643.68a7aeb1.chunk.js.map
