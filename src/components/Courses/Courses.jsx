9ca61d739575965410f73a901f731d1afa67eb24
VladOlevskiy <99341001+VladOlevskiy@users.noreply.github.com>
2023-06-01 12:33:57 +0300

    fix

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -46,19 +46,6 @@ const TextAnimationDawn = {
     duration: 1,
   },
 };
-const TextAnimationUp = {
-  offscreen: {
-    y: -300,
-    opacity: 0,
-  },
-  onscreen: {
-    y: 0,
-    opacity: 1,
-    transition: { type: 'spring' },
-    bounce: 0.2,
-    duration: 3,
-  },
-};
 
 const TextAnimationLeft = {
   offscreen: {
